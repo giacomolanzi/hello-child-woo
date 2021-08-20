cbe06d856f10204e37bb821572dd906ad42081fa
Giacomo Lanzi <50149567+giacomolanzi@users.noreply.github.com>
2021-08-20 12:07:31 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -7,16 +7,62 @@
 /**
  * Load child theme css and optional scripts
  * @return void
- * Remeber to set version number to fixed before productio. Es.: '1.2.3'
  */
 function hello_elementor_child_enqueue_scripts() {
-	wp_enqueue_style(
-		'hello-elementor-child-style', // Name of the style. aka Link Element's ID name
-		get_stylesheet_directory_uri() . '/style.css', // URI
+    $version = rand(111,999);
+    wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
 		[
-			'hello-elementor-theme-style', // Dependencies
+			'hello-elementor-theme-style',
 		],
-		rand(111,999) // Version
+       $version 
 	);
+    wp_enqueue_style('wc-custom-style',
+        get_stylesheet_directory_uri() . '/wc-custom-style.css',
+        array(),
+       $version 
+    );
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+require_once(__DIR__ . '/includes/security.php');
+
+// Includere supporto per woocommerce
+add_theme_support('woocommerce');
+
+// Shortcode to place *post content* where you want
+function wpc_elementor_shortcode( $atts ) {
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+	the_content();
+	endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+	<?php endif;
+}
+add_shortcode( 'my_elementor_postcontent', 'wpc_elementor_shortcode');
+
+// Shortcode to place post content where you want, but shorter
+function wpc_elementor_shortcode_2( $atts ) {
+	echo get_post_field('post_content', $post->ID);
+}
+add_shortcode( 'my_elementor_postcontent_shorter', 'wpc_elementor_shortcode_2');
+
+/**
+*  Aggiungi icone personalizzate ai gataway di pagamento 
+*/ 
+function custom_gateway_icon( $icon, $id ) {
+    if ( $id === 'stripe' ) {
+        return '<img src="' . get_stylesheet_directory_uri() . '/includes/stripe.svg' . '" alt="Stripe" width="50" > 
+                <img src="' . get_stylesheet_directory_uri() . '/includes/visa.svg' . '" alt="Visa" width="50" > 
+                <img src="' . get_stylesheet_directory_uri() . '/includes/mastercard.svg' . '" alt="Mastercard" width="50" >
+        '; 
+    } elseif ($id === 'bacs') {
+        return '<img src="' . get_stylesheet_directory_uri() . '/includes/bank.svg' . '" alt="Bank Transfert" width="30" >
+        ';
+    } elseif ($id === 'paypal') {
+        return '<img src="' . get_stylesheet_directory_uri() . '/includes/paypal.svg' . '" alt="Bank Transfert" width="50" >';
+    }else {
+        return $icon;
+    }
+}
+add_filter( 'woocommerce_gateway_icon', 'custom_gateway_icon', 10, 2 );
